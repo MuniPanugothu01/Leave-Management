b88b4f59d15c514b35e9a2b03e1d7679039d8a38
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-03-24 15:04:01 +0530

    imported the bootstrap cdn links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import LeaveRequest from './LeaveRequest'
+import LeaveRequest from './LeaveRequest';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 function App() {
 
   return (
