4f9881dc50b1c0734ba91ef582b60b2c31f9b478
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-03-25 12:49:14 +0530

    style.css file imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import LeaveRequest from './LeaveRequest';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 // import ListEmployee from './Crud';
 import LeaveRequestComponent from './LeaveRequest'
 function App() {
